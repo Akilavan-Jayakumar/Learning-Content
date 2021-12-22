8f42fc7e9a4dd1c8ed217e44d00c265d5362514c
Akilavan-Jayakumar <79683904+Akilavan-Jayakumar@users.noreply.github.com>
2021-12-22 16:44:35 +0530

    Update insert_single_row.js

--- Node-JS/Datastore/insert_single_row.js
+++ Node-JS/Datastore/insert_single_row.js
@@ -1,4 +1,13 @@
-console.log("Hi");
-console.log("hj");
-var a =10;
-console.log(a)
+
+  let rowData = 
+    { 
+        Name: `George Hamilton`,
+        Age: 22,
+        ID: 6868
+    };
+    let datastore = app.datastore();
+    let table = datastore.table('EmpDetails');
+    let insertPromise = table.insertRow(rowData);
+    insertPromise.then((row) => {
+            console.log(row);
+    });
