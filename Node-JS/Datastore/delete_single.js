e994dd496982afd060d9476e423a4fb1419969c4
Akilavan-Jayakumar <79683904+Akilavan-Jayakumar@users.noreply.github.com>
2022-01-03 10:24:39 +0530

    Update delete_single.js

--- Node-JS/Datastore/delete_single.js
+++ Node-JS/Datastore/delete_single.js
@@ -5,25 +5,25 @@ const app = express();
 app.use(express.json());
 
 app.delete("/single", async (req, res) => {
-	try {
-	  const restaurant_id = req.query.restaurant_id;
-	  const catalyst = res.locals.catalyst;
-	  const table = catalyst.datastore().table("Restaurants");
-	  await table.deleteRow(restaurant_id);
-	  res.status(200).send({
-		code: 2000,
-		info: "Deleted the row successfully",
-		data: {},
-	  });
-	} catch (err) {
-	  console.log(err);
-	  res.status(500).send({
-		err,
-		code: 5000,
-		info: "Some error occurred",
-		data: {},
-	  });
-	}
-  });
+  try {
+    const restaurant_id = req.query.restaurant_id;
+    const catalyst = catalystSDK.initialize(req);
+    const table = catalyst.datastore().table("Restaurants");
+    await table.deleteRow(restaurant_id);
+    res.status(200).send({
+      code: 2000,
+      info: "Deleted the row successfully",
+      data: {},
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      err,
+      code: 5000,
+      info: "Some error occurred",
+      data: {},
+    });
+  }
+});
 
 module.exports = app;
