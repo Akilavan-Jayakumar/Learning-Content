964deda05a1201e2148cc2ab805b543cd39de3d9
Akilavan-Jayakumar <79683904+Akilavan-Jayakumar@users.noreply.github.com>
2022-01-03 10:56:55 +0530

    Update pagination.js

--- Node-JS/Datastore/pagination.js
+++ Node-JS/Datastore/pagination.js
@@ -24,7 +24,8 @@ app.get("/all", async (req, res) => {
       }
       const startIndex = currentPage * rowsPerPage + 1;
       const dataQuery = `SELECT Restaurants.ROWID,Restaurants.name,Restaurants.phone,
-                         Restaurants.city,Restaurants.rating,Restaurants.email FROM Restaurants 
+                         Restaurants.city,Restaurants.rating,Restaurants.email 
+                         FROM Restaurants 
                          LIMIT ${startIndex},${rowsPerPage}`;
       const dataResponse = await zcql.executeZCQLQuery(dataQuery);
       res.status(200).send({
